bbf7084fd36dfec25abf77d212c4f0d06427ed05
Jenice Edmonds <jenice54@icloud.com>
2022-08-02 16:24:55 -0400

    wild wild life answered

--- homework-files/W2D2HW/answers.js
+++ homework-files/W2D2HW/answers.js
@@ -23,7 +23,7 @@
 
 
 //FIZZ BUZZ
-
+/*
 for(i = 0; i <= 100; i++){
   
   if(i % 15 === 0){
@@ -35,5 +35,49 @@ for(i = 0; i <= 100; i++){
   }else{
     console.log(i);
   } 
-  }
+  }*/
    // git commit -m " FizzBuzz Answered"
+
+/*
+   WILD WILD LIFE
+
+
+Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+
+
+
+Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+
+
+
+Give D'Art a second hometown by adding "Hawkins"
+
+
+
+Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+
+*/
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] = 5001;
+
+console.log(plantee);
+
+wolfy[3] = "Gotham City";
+
+console.log(wolfy);
+
+dart.push("Hawkins");
+
+console.log(dart);
+
+wolfy[0] = "Gameboy";
+
+console.log(wolfy);
+
+//git commit -m "Completed wild wild life "
+
