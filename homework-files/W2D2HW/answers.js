09df797d26dce4213484c35ae9a43330b8db3b1d
Jenice Edmonds <jenice54@icloud.com>
2022-08-02 16:22:40 -0400

    fizz buzz answered

--- homework-files/W2D2HW/answers.js
+++ homework-files/W2D2HW/answers.js
@@ -8,11 +8,32 @@
 
     // console.log(i);
 
+
+/*
+  Even Numbers 
+  
+  
     for(i = 0; i <= 200; i++){
         let evenNumber = (i % 2 === 0)
         if(evenNumber){
             console.log(i);
         }
       }
+*/
 
 
+//FIZZ BUZZ
+
+for(i = 0; i <= 100; i++){
+  
+  if(i % 15 === 0){
+      console.log("Fizz-Buzz");
+  }else if(i % 3 === 0){
+    console.log("Fizz");
+  }else if(i % 5 === 0){
+    console.log("Buzz");
+  }else{
+    console.log(i);
+  } 
+  }
+   // git commit -m " FizzBuzz Answered"
