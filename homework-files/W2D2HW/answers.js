02152a63647895f824e2f65b5c5f9a0b9b370c70
Jenice Edmonds <jenice54@icloud.com>
2022-08-02 16:17:41 -0400

    Even numbers Answered

--- homework-files/W2D2HW/answers.js
+++ homework-files/W2D2HW/answers.js
@@ -2,10 +2,17 @@
 
    // Write a for loop that will log the numbers 1 through 20.
     
-                for(i = 0; i <= 20; i++){
-        console.log(i);
-    }
+    //             for(i = 0; i <= 20; i++){
+    //     console.log(i);
+    // }
 
-    console.log(i);
+    // console.log(i);
+
+    for(i = 0; i <= 200; i++){
+        let evenNumber = (i % 2 === 0)
+        if(evenNumber){
+            console.log(i);
+        }
+      }
 
 
